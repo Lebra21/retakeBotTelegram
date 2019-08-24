acf8df6b2ece2c3d71ec4e88e8c2de0217064b78
Zhaslan <zkaragoyshin@gmail.com>
2019-08-24 16:59:44 +0500

    initial

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import telebot
 TOKEN = '929241007:AAFrhbemrhXy-N8IVA0e0qy6dCMLYxHkj0Q'
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+
+bot.polling()
