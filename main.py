0400c7e42dbeecee2d6378e933d757b5a059a3bf
Zhaslan <zkaragoyshin@gmail.com>
2019-08-24 18:19:54 +0500

    second commit

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ TOKEN = '929241007:AAFrhbemrhXy-N8IVA0e0qy6dCMLYxHkj0Q'
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
 
+@bot.message_handler(commands=['help'])
+def send_helper(message):
+	bot.reply_to(message,'Botik idi nahui')
 
 bot.polling()
