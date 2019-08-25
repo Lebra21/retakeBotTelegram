2bee74b5efa79dbc39e83114cfefcc2d8ab660fe
Zhaslan <zkaragoyshin@gmail.com>
2019-08-25 22:08:39 +0500

    ssss

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 import telebot , constants , random , os , time
-from telebot.types import Message
+from telebot.types import Message , ReplyKeyboardRemove
 
 
 bot = telebot.TeleBot(constants.TOKEN)
-keyboard = telebot.types.ReplyKeyboardMarkup(True,True)
-keyboard.row('Alisher is monkey?','/help')
+
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Welcome!",reply_markup=keyboard)
+    user_markup = telebot.types.ReplyKeyboardMarkup()
+    user_markup.row('/start','/stop')
+    user_markup.row('photo','audio','media')
+    user_markup.row('sticker','video','voice','location')
+    bot.send_message(message.from_user.id,'Welcome!',reply_markup=user_markup)
+
+@bot.message_handler(commands=['stop'])
+def send_stop(message):
+    hide_markup = telebot.types.ReplyKeyboardRemove()
+    bot.send_message(message.from_user.id,'...',reply_markup=hide_markup)
 
 @bot.message_handler(commands=['help'])
 def send_helper(message):
-	bot.reply_to(message,"Darkhan idi nahui")
+	bot.reply_to(message,"Darkhan 0 help")
 
 # Обработчик для документов и аудиофайлов
 
-
- # Обработчик сообщений, содержащих документ с mime_type 'text/plain' (обычный текст)
-@bot.message_handler(func=lambda message: True)
-def send_smile(message: Message):
-    if 'Alisher is monkey?' in message.text:
-        bot.reply_to(message, random.choice(constants.smiles))
-        return
-    else:
-        pass
-
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text == 'photo':
+        directory = 'C:/Users/user/PycharmProjects/retakersBotTelegram/files/photos'
+        all_files_in_directory = os.listdir(directory)
+        random_file = random.choice(all_files_in_directory)
+        img = open(directory + '/' + random_file,'rb')
+        bot.send_chat_action(message.from_user.id,'upload_photo')
+        bot.send_photo(message.from_user.id,img)
+        img.close()
+    elif message.text == 'voice':
+        voice = open("C:/Users/user/PycharmProjects/retakersBotTelegram/files/voice/Папич-—-Здарова-_www.mixmuz.ru_.ogg",'rb')
+        bot.send_chat_action(message.from_user.id, 'upload_voice')
+        bot.send_voice(message.from_user.id, voice)
+        voice.close()
+    elif message.text == 'location':
+        bot.send_chat_action(message.from_user.id,'upload location')
+        bot.send_location(message.from_user.id,43.207836,76.669086)
 
 while True:
     try:
